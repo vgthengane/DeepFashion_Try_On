4cb6cc6d358231b38798af1cc46d03dfdb36e75f
VishalGT <vishal@smartail.ai>
2021-01-11 16:24:06 +0530

    added torch.nn.parallel.DistributedDataParallel

--- ACGPN_inference/models/models.py
+++ ACGPN_inference/models/models.py
@@ -13,6 +13,6 @@ def create_model(opt):
         print("model [%s] was created" % (model.name()))
 
     if opt.isTrain and len(opt.gpu_ids):
-        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=opt.gpu_ids)
 
     return model
